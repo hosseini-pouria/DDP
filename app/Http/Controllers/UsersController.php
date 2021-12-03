b6027cf76c39f0df74b2a4498f8a2d9638b65719
Pouria <seydpooryahoseini@gmail.com>
2021-12-03 21:54:48 +0330

    create UsersController.php

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UsersController extends Controller
+{
+    public function updateWallet(Request $request)
+    {
+        $user = User::find(1);
+//        $user_wallet = $user->wallet;
+//        $user_wallet += 100000;
+//        $user->wallet = $user_wallet;
+//        $user->save();
+//        $user->update(['wallet', $user_wallet]);
+        $user->increaseWallet(100000);
+    }
+}
