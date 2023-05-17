09e2bbe10dd2fcea95946962ca2760b4562bc96e
pouria hosseini <seydpooryahoseini@gmail.com>
2023-05-17 16:33:01 +0330

    Done chached Repository for proxy pattern

---
+++ src/CachedRepository/Client.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Src\CachedRepository;
+
+class Client
+{
+    private ProductRepositoryInterface $productRepository;
+
+    /**
+     * @param ProductRepositoryInterface $productRepository
+     */
+    public function __construct(ProductRepositoryInterface $productRepository)
+    {
+        $this->productRepository = $productRepository;
+    }
+}
