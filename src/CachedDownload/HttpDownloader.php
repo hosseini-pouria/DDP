c6fb62a14bbfdda52266dbd514011eef0a3effbb
pouria hosseini <seydpooryahoseini@gmail.com>
2023-05-20 15:46:17 +0330

    Done cached download for proxy pattern

---
+++ src/CachedDownload/HttpDownloader.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Src\CachedDownload;
+
+class HttpDownloader implements Downloader
+{
+
+    public function download(string $url)
+    {
+        // TODO: Implement download() method.
+    }
+}
